efa11b63cb10ffe1264d5cc40720ea1b4ec996de
M.Drolet <matthisdrolet@gmail.com>
2017-04-17 17:52:17 +0200

    My first commit

--- FreakingApp01/ViewController.swift
+++ FreakingApp01/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapButtonCount = 0
+    var tapChangeCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapButtonCount += 1
+        
+        if tapButtonCount >= 10 {
+            theLabel.text = "Button taps maxed out!"
+        } else if theLabel.text == "How you doin'??" {
+            theLabel.text = "Me?"
+        } else {
+            theLabel.text = "How you doin'??"
+        }
+        
+        print("Button tapped \(tapButtonCount) times")
+    }
+
+    @IBAction func changeTapped(_ sender: Any) {
+        
+        tapChangeCount += 1
+        
+        if tapChangeCount >= 10 {
+            theLabel.text = "Change taps maxed out!"
+        } else if theLabel.text == "How you doin'??" {
+            theLabel.text = "I want to change the button!"
+        } else {
+            theLabel.text = "Buttons are cool!"
+        }
+        
+        print("Change tapped \(tapChangeCount) times")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //self.view.backgroundColor = UIColor.red
+        
     }
 
     override func didReceiveMemoryWarning() {
